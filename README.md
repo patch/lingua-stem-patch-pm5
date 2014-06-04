e64b26acdc85010479dd97e6bb66c093cce8a575
Nick Patch <patch@cpan.org>
2014-06-03 21:57:00 -0400

    new project: Lingua::Stem::Patch

---
+++ README.md
@@ -0,0 +1,28 @@
+# NAME
+
+Lingua::Stem::Patch - Patch stemmers
+
+# VERSION
+
+This document describes Lingua::Stem::Patch v0.00\_1.
+
+# SYNOPSIS
+
+```perl
+use Lingua::Stem::Patch;
+```
+
+# DESCRIPTION
+
+Patch stemmers.
+
+# AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+© 2014 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
---
+++ lib/Lingua/Stem/Patch.pm
@@ -0,0 +1,41 @@
+package Lingua::Stem::Patch;
+
+use v5.8.1;
+use utf8;
+use strict;
+use warnings;
+
+our $VERSION = '0.00_1';
+
+1;
+
+__END__
+
+=encoding UTF-8
+
+=head1 NAME
+
+Lingua::Stem::Patch - Patch stemmers
+
+=head1 VERSION
+
+This document describes Lingua::Stem::Patch v0.00_1.
+
+=head1 SYNOPSIS
+
+    use Lingua::Stem::Patch;
+
+=head1 DESCRIPTION
+
+Patch stemmers.
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+© 2014 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
